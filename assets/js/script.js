8b865b3519543de98394c60084be3141d210b007
billhamilton68 <billhamilton68@gmail.com>
2023-03-19 10:38:10 -0700

    update 3 hiding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,22 @@
 const timer = document.querySelector('#timer');
 const startBtn = document.querySelector('#start-quiz-btn');
-
+const ans1Btn = document.querySelector('#ans1');
+const ans2Btn = document.querySelector('#ans2');
+const ans3Btn = document.querySelector('#ans3');
+const ans4Btn = document.querySelector('#ans4');
+const introtxt = document.querySelector("p");
 startBtn.addEventListener('click', startTimer);
 
 let startButton = document.querySelector('button');
 
 startButton.onclick = function() {
   //startButton.style.backgroundColor = "magenta";
-  startButton.style.visibility = "hidden"
+  startButton.style.visibility = "hidden";
+  ans1Btn.style.visibility = "visible";
+  ans2Btn.style.visibility = "visible";
+  ans3Btn.style.visibility = "visible";
+  ans4Btn.style.visibility = "visible";
+   introtxt.style.visibility = "hidden";
 };
 
 function startTimer() {
