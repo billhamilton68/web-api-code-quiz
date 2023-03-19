15646d078fbb6e507a6484e0fc65e0842e7c8427
billhamilton68 <billhamilton68@gmail.com>
2023-03-19 08:25:56 -0700

    update 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,13 @@ const startBtn = document.querySelector('#start-quiz-btn');
 
 startBtn.addEventListener('click', startTimer);
 
+let startButton = document.querySelector('button');
+
+startButton.onclick = function() {
+  //startButton.style.backgroundColor = "magenta";
+  startButton.style.visibility = "hidden"
+};
+
 function startTimer() {
   let timeLeft = 60;
   const timerInterval = setInterval(() => {
