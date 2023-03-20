f267d3da57db8915f480b0582498dfe47b3c64db
billhamilton68 <billhamilton68@gmail.com>
2023-03-20 06:07:52 -0700

    startover :(

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,56 +7,36 @@ const ans4Btn = document.querySelector('#ans4');
 const introtxt = document.querySelector("#intro");
 startBtn.addEventListener('click', startTimer);
 
+var questions = ["What is Tucker's favorie lunch?",
+"What is the keyword used for Variable?",
+"What does full name of HTML?","Which charecter has to be entered at the end of java script statements?"];
+
+var answers = ["1. Taco","2. Cheeseburger and fries","3. Mac and Cheese","4. Pizza","1. Var","2. var","3. virtual","4. Vincent",
+"1. Having Time and Managing Life","2. Hyper Text Markup Language","3. Half Time Minnesota Loses","4. Happy Terrific Markup Language",
+"1. #","2. $","3. {}","4. ;",];
+
 var introText = "Try to answer the following questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!"
 
-var question = ""
-const question1 = "What is Tucker's favorie lunch?"
-const q1Answer = "Taco";
-const q1Option1 = "Taco";
-const q1Option2 = "Cheeseburger and fries";
-const q1Option3 = "Mac and Cheese";
-const q1Option4 = "Pizza";
-
-
-const question2 = "What is the keyword used for Variable?";
-const q2Answer = "var";
-const q2Option1 = "Var";
-const q2Option2 = "var";
-const q2Option3 = "virtual";
-const q2Option4 = "Vincent";
-
-const question3 = "What does full name of HTML?";
-const q3Answer = "Hyper Text Markup Language";
-const q3Option1 = "Having Time and Managing Life";
-const q3Option2 = "Hyper Text Markup Language";
-const q3Option3 = "Half Time Minnesota Loses";
-const q3Option4 = "Happy Terrific Markup Language";
-
-const question4 = "Which charecter has to be entered at the end of java script statements?";
-const q4Answer = ";";
-const q4Option1 = "#";
-const q4Option2 = "$";
-const q4Option3 = "{}";
-const q4Option4 = ";";
+var question = "";
 
 let startButton = document.querySelector('button');
 
 startButton.onclick = function() {
   //startButton.style.backgroundColor = "magenta";
   startButton.style.visibility = "hidden";
-   ans1Btn.innerHTML = "1. " + q1Option1;
-  ans2Btn.innerHTML = "2. " + q1Option2;
-  ans3Btn.innerHTML = "3. " + q1Option3;
-  ans4Btn.innerHTML = "4. " + q1Option4;
-  ans1Btn.style.visibility = "visible";
-  ans2Btn.style.visibility = "visible";
-  ans3Btn.style.visibility = "visible";
-  ans4Btn.style.visibility = "visible";
+
   introtxt.style.visibility = "hidden";
-  question = question1
+  document.querySelector("#question").innerHTML = questions[0];
+  askQuestion();
    document.querySelector("#question").style.visibility = "visible";
-   document.querySelector("#question").innerHTML = question;
    
+   
+};
+
+function askQuestion (){
+
+  return questions[0];
+
 };
 
 function startTimer() {
@@ -65,10 +45,11 @@ function startTimer() {
     if (timeLeft <= 0) {
       clearInterval(timerInterval);
       alert('Time is up!');
-      
+      window.location.href = window.location.href;
     } else {
       timeLeft--;
       timer.textContent = `Timer: ${timeLeft}`;
     }
   }, 1000);
+ 
 }
